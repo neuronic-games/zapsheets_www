feb16cabf4bc1d89a70d948db26cc10c9ee1c1e9
3wc <3wc.github@doesthisthing.work>
2025-12-02 21:58:39 -0500

    test: Working feature/system/end-to-end tests..
    ..for /, and /push/

---
+++ tests/EndToEnd/MapTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use function Playwright\Testing\expect;
+
+require_once __DIR__ . '/../helpers/TestHelper.php';
+
+/**
+ * Test main application functionality
+ */
+class MapTest extends TestHelper
+{
+    public function testIndex(): void
+    {
+        $this->navigateTo('/?id=' . $_ENV['TEST_SPREADSHEET_ID']);
+
+        $this->waitForElement("#defaultScreen", 5 * 1000);
+
+        $this->page->locator("#directoryContainer")->waitFor(['state' => 'visible']);
+
+        $this->page->locator("#spinningLoader")->waitFor(['state' => 'hidden']);
+
+        $this->page->locator("#splashScreen")->waitFor(['state' => 'hidden']);
+
+        expect($this->page->locator("#mapListContainer div div:first-child"))
+          ->withTimeout(20 * 1000)
+          ->toContainText("Admissions");
+
+        $this->page->screenshot(getScreenshotPath(get_class($this) . '::MapTest'));
+    }
+}
---
+++ tests/EndToEnd/PushTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use function Playwright\Testing\expect;
+
+require_once __DIR__ . '/../helpers/TestHelper.php';
+
+/**
+ * Test main application functionality
+ */
+class PushTest extends TestHelper
+{
+    public function testIndex(): void
+    {
+        $this->navigateTo('/push/?id=' . $_ENV['TEST_SPREADSHEET_ID']);
+
+        expect($this->page->locator("#loadingTxt"))
+          ->withTimeout(2 * 60 * 1000) // 2 minutes
+          ->toContainText("All data published");
+
+        $this->page->screenshot(getScreenshotPath(get_class($this) . '::PushTest'));
+    }
+}
