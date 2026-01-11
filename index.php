95b237629b1ac951dd7b9a11125717226a05e578
3wc <3wc.github@doesthisthing.work>
2026-01-11 15:46:10 -0500

    style: PHP autoformatter on index.php

--- index.php
+++ index.php
@@ -12,8 +12,9 @@
   <meta name="apple-mobile-web-app-capable" content="yes" />
   <title>Map</title>
   <link rel="stylesheet" href="./css/bootstrap.min.css"
-    <?php if($_ENV['ENVIRONMENT'] != 'development') {
-      echo 'integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"';} ?>
+    <?php if ($_ENV['ENVIRONMENT'] != 'development') {
+        echo 'integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"';
+    } ?>
       crossorigin="anonymous" />
   <!-- <link rel="stylesheet" href="css/style.css"/> -->
   <link rel="icon" type="image/x-icon" href="./images/icon_map.png" />
@@ -22,24 +23,29 @@
 </head>
 
 <script src="./js-package/bootstrap.bundle.min.js"
-  <?php if($_ENV['ENVIRONMENT'] != 'development') {
-    echo 'integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"';} ?>
+  <?php if ($_ENV['ENVIRONMENT'] != 'development') {
+      echo 'integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"';
+  } ?>
     crossorigin="anonymous"></script>
 <script src="./js-package/jquery-3.5.1.min.js"
-  <?php if($_ENV['ENVIRONMENT'] != 'development') {
-    echo 'integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="';} ?> crossorigin="anonymous"></script>
+  <?php if ($_ENV['ENVIRONMENT'] != 'development') {
+      echo 'integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="';
+  } ?> crossorigin="anonymous"></script>
 <script src="./slick/slick.js"></script>
 <script src="./js-package/moment.min.js"
-  <?php if($_ENV['ENVIRONMENT'] != 'development') {
-    echo 'integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ=="';} ?>
+  <?php if ($_ENV['ENVIRONMENT'] != 'development') {
+      echo 'integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ=="';
+  } ?>
     crossorigin="anonymous"></script>
 <script src="./js-package/popper.min.js"
-  <?php if($_ENV['ENVIRONMENT'] != 'development') {
-    echo 'integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"';} ?>
+  <?php if ($_ENV['ENVIRONMENT'] != 'development') {
+      echo 'integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"';
+  } ?>
     crossorigin="anonymous"></script>
 <script src="./js-package/bootstrap.min.js"
-  <?php if($_ENV['ENVIRONMENT'] != 'development') {
-    echo 'integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"'; } ?>
+  <?php if ($_ENV['ENVIRONMENT'] != 'development') {
+      echo 'integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"';
+  } ?>
     crossorigin="anonymous"></script>
 <script src="./js-package/qrcode.min.js"></script>
 <!-- New Preloader-->
